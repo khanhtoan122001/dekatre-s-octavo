0f8f2862711d4a1ad3af7798f9b9d35823b5b352
khanhtoan122001 <khanhtoannguyen12@gmail.com>
2021-11-09 09:54:53 +0700

    form đăng nhập (demo)

--- dekatreís-octavo/dekatreís-octavo/Form1.cs
+++ dekatreís-octavo/dekatreís-octavo/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +12,28 @@ using System.Windows.Forms;
 
 namespace dekatreís_octavo
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
+
+            // Create a material theme manager and add the form to manage (this)
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue400, Primary.Blue500,
+                Primary.Blue500, Accent.LightBlue200,
+                TextShade.WHITE
+            );
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
