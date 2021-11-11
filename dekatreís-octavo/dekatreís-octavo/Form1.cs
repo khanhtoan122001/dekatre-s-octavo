5775d2b4c7c7d771665551fb0cf190eb24f33a62
yami-newbie <68190399+yami-newbie@users.noreply.github.com>
2021-11-12 00:05:31 +0700

    Revert "Merge remote-tracking branch 'origin/toan' into test"
    This reverts commit 68773b474a499dfbed3c578b599384adec1ff880, reversing
    changes made to 5beca0f1bdb7c435c61bed7d8006e6f5332f2828.

---
+++ dekatreís-octavo/dekatreís-octavo/Form1.cs
@@ -0,0 +1,40 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dekatreís_octavo
+{
+    public partial class Form1 : MaterialForm
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Create a material theme manager and add the form to manage (this)
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue400, Primary.Blue500,
+                Primary.Blue500, Accent.LightBlue200,
+                TextShade.WHITE
+            );
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _ = DataAccess.ExecuteQuery.Instance;
+        }
+    }
+}
