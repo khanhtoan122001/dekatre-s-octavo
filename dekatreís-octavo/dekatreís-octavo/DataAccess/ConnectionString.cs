1c650282f7570ef727c8ea411ddcfa882f1e3e77
khanhtoan122001 <khanhtoannguyen12@gmail.com>
2021-11-12 00:49:30 +0700

    refresh

--- dekatreís-octavo/dekatreís-octavo/DataAccess/ConnectionString.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace dekatreís_octavo.DataAccess
-{
-    public class ConnectionString
-    {
-        static public ConnectionString Instance;
-        string connectionString = "";
-    }
-}
