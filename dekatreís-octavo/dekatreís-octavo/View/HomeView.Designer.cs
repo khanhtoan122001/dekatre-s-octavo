065493e088db7f45fcc43453a9aa4f773030db3b
khanhtoan122001 <khanhtoannguyen12@gmail.com>
2021-11-12 00:10:23 +0700

    Update HomeView.Designer.cs

--- dekatreís-octavo/dekatreís-octavo/View/HomeView.Designer.cs
+++ dekatreís-octavo/dekatreís-octavo/View/HomeView.Designer.cs
@@ -33,6 +33,11 @@ namespace dekatreís_octavo
             this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.materialCard1 = new MaterialSkin.Controls.MaterialCard();
+            this.tabControl = new MaterialSkin.Controls.MaterialDrawer();
             this.materialTabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,7 +51,7 @@ namespace dekatreís_octavo
             this.materialDrawer1.HighlightWithAccent = true;
             this.materialDrawer1.IndicatorWidth = 0;
             this.materialDrawer1.IsOpen = false;
-            this.materialDrawer1.Location = new System.Drawing.Point(1, 83);
+            this.materialDrawer1.Location = new System.Drawing.Point(-145, 83);
             this.materialDrawer1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialDrawer1.Name = "materialDrawer1";
             this.materialDrawer1.ShowIconsWhenHidden = false;
@@ -59,6 +64,9 @@ namespace dekatreís_octavo
             // 
             this.materialTabControl1.Controls.Add(this.tabPage1);
             this.materialTabControl1.Controls.Add(this.tabPage2);
+            this.materialTabControl1.Controls.Add(this.tabPage3);
+            this.materialTabControl1.Controls.Add(this.tabPage4);
+            this.materialTabControl1.Controls.Add(this.tabPage5);
             this.materialTabControl1.Depth = 0;
             this.materialTabControl1.Location = new System.Drawing.Point(152, 54);
             this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
@@ -88,11 +96,74 @@ namespace dekatreís_octavo
             this.tabPage2.Text = "Nhân viên";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(642, 367);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Báo cáo";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(642, 367);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Cài đặt";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(642, 367);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Tình trạng bãi";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // materialCard1
+            // 
+            this.materialCard1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialCard1.Depth = 0;
+            this.materialCard1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialCard1.Location = new System.Drawing.Point(609, 6);
+            this.materialCard1.Margin = new System.Windows.Forms.Padding(14);
+            this.materialCard1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialCard1.Name = "materialCard1";
+            this.materialCard1.Padding = new System.Windows.Forms.Padding(14);
+            this.materialCard1.Size = new System.Drawing.Size(189, 31);
+            this.materialCard1.TabIndex = 2;
+            // 
+            // tabControl
+            // 
+            this.tabControl.AutoHide = false;
+            this.tabControl.AutoShow = false;
+            this.tabControl.BackColor = System.Drawing.Color.White;
+            this.tabControl.BackgroundWithAccent = false;
+            this.tabControl.BaseTabControl = null;
+            this.tabControl.Depth = 0;
+            this.tabControl.HighlightWithAccent = true;
+            this.tabControl.IndicatorWidth = 0;
+            this.tabControl.IsOpen = false;
+            this.tabControl.Location = new System.Drawing.Point(-1, 83);
+            this.tabControl.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tabControl.Name = "tabControl";
+            this.tabControl.ShowIconsWhenHidden = false;
+            this.tabControl.Size = new System.Drawing.Size(153, 367);
+            this.tabControl.TabIndex = 3;
+            this.tabControl.Text = "materialDrawer2";
+            this.tabControl.UseColors = false;
+            // 
             // HomeView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.materialCard1);
             this.Controls.Add(this.materialTabControl1);
             this.Controls.Add(this.materialDrawer1);
             this.Name = "HomeView";
@@ -108,5 +179,10 @@ namespace dekatreís_octavo
         private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private MaterialSkin.Controls.MaterialCard materialCard1;
+        private MaterialSkin.Controls.MaterialDrawer tabControl;
     }
 }
