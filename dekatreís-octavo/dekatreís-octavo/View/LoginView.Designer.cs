0820873a497c11a95fe0db1e560545c970b95834
khanhtoan122001 <khanhtoannguyen12@gmail.com>
2021-11-12 01:31:59 +0700

    +1 ui

---
+++ dekatreís-octavo/dekatreís-octavo/View/LoginView.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace dekatreís_octavo.View
+{
+    partial class LoginView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.materialTextfield1 = new MaterialSurface.MaterialTextfield();
+            this.materialTextfield2 = new MaterialSurface.MaterialTextfield();
+            this.containedButton1 = new MaterialSurface.ContainedButton();
+            this.materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(401, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // materialTextfield1
+            // 
+            this.materialTextfield1.AutoScaleColor = true;
+            this.materialTextfield1.BackColor = System.Drawing.SystemColors.Control;
+            this.materialTextfield1.CountText = false;
+            this.materialTextfield1.FieldType = MaterialSurface.BoxType.Normal;
+            this.materialTextfield1.FloatingLabelText = "FloatingLabel";
+            this.materialTextfield1.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.materialTextfield1.HandleError = false;
+            this.materialTextfield1.HelperText = "";
+            this.materialTextfield1.HideSelection = true;
+            this.materialTextfield1.HintText = "TĐN";
+            this.materialTextfield1.Location = new System.Drawing.Point(488, 141);
+            this.materialTextfield1.MaxLength = 32767;
+            this.materialTextfield1.MouseState = MaterialSurface.MouseState.OUT;
+            this.materialTextfield1.Multiline = false;
+            this.materialTextfield1.Name = "materialTextfield1";
+            this.materialTextfield1.PasswordChar = '\0';
+            this.materialTextfield1.PrimaryColor = System.Drawing.Color.SteelBlue;
+            this.materialTextfield1.ReadOnly = false;
+            this.materialTextfield1.ShortcutsEnable = true;
+            this.materialTextfield1.ShowCaret = true;
+            this.materialTextfield1.Size = new System.Drawing.Size(216, 49);
+            this.materialTextfield1.Style = MaterialSurface.MaterialTextfield.TextfieldStyle.HintAsFloatingLabel;
+            this.materialTextfield1.TabIndex = 1;
+            this.materialTextfield1.UseSystemPasswordChar = false;
+            // 
+            // materialTextfield2
+            // 
+            this.materialTextfield2.AutoScaleColor = true;
+            this.materialTextfield2.BackColor = System.Drawing.SystemColors.Control;
+            this.materialTextfield2.CountText = false;
+            this.materialTextfield2.FieldType = MaterialSurface.BoxType.Normal;
+            this.materialTextfield2.FloatingLabelText = "FloatingLabel";
+            this.materialTextfield2.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.materialTextfield2.HandleError = false;
+            this.materialTextfield2.HelperText = "";
+            this.materialTextfield2.HideSelection = true;
+            this.materialTextfield2.HintText = "MK";
+            this.materialTextfield2.Location = new System.Drawing.Point(488, 209);
+            this.materialTextfield2.MaxLength = 32767;
+            this.materialTextfield2.MouseState = MaterialSurface.MouseState.OUT;
+            this.materialTextfield2.Multiline = false;
+            this.materialTextfield2.Name = "materialTextfield2";
+            this.materialTextfield2.PasswordChar = '●';
+            this.materialTextfield2.PrimaryColor = System.Drawing.Color.SteelBlue;
+            this.materialTextfield2.ReadOnly = false;
+            this.materialTextfield2.ShortcutsEnable = true;
+            this.materialTextfield2.ShowCaret = true;
+            this.materialTextfield2.Size = new System.Drawing.Size(216, 49);
+            this.materialTextfield2.Style = MaterialSurface.MaterialTextfield.TextfieldStyle.HintAsFloatingLabel;
+            this.materialTextfield2.TabIndex = 2;
+            this.materialTextfield2.UseSystemPasswordChar = true;
+            // 
+            // containedButton1
+            // 
+            this.containedButton1.EffectType = MaterialSurface.ET.Custom;
+            this.containedButton1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.containedButton1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.containedButton1.Icon = null;
+            this.containedButton1.Location = new System.Drawing.Point(488, 293);
+            this.containedButton1.MouseState = MaterialSurface.MouseState.OUT;
+            this.containedButton1.Name = "containedButton1";
+            this.containedButton1.PrimaryColor = System.Drawing.Color.SteelBlue;
+            this.containedButton1.Radius = 6;
+            this.containedButton1.ShawdowDepth = 3;
+            this.containedButton1.ShawdowOpacity = 50;
+            this.containedButton1.Size = new System.Drawing.Size(216, 51);
+            this.containedButton1.TabIndex = 3;
+            this.containedButton1.Text = "Đăng nhập";
+            this.containedButton1.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.containedButton1.UseVisualStyleBackColor = true;
+            // 
+            // materialButton1
+            // 
+            this.materialButton1.AutoSize = false;
+            this.materialButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton1.BackColor = System.Drawing.SystemColors.Control;
+            this.materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton1.Depth = 0;
+            this.materialButton1.DrawShadows = false;
+            this.materialButton1.HighEmphasis = false;
+            this.materialButton1.Icon = null;
+            this.materialButton1.Location = new System.Drawing.Point(752, 0);
+            this.materialButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton1.Name = "materialButton1";
+            this.materialButton1.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton1.Size = new System.Drawing.Size(50, 36);
+            this.materialButton1.TabIndex = 4;
+            this.materialButton1.Text = "X";
+            this.materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton1.UseAccentColor = false;
+            this.materialButton1.UseVisualStyleBackColor = false;
+            // 
+            // LoginView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.materialButton1);
+            this.Controls.Add(this.containedButton1);
+            this.Controls.Add(this.materialTextfield2);
+            this.Controls.Add(this.materialTextfield1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginView";
+            this.Text = "LoginView";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MaterialSurface.MaterialTextfield materialTextfield1;
+        private MaterialSurface.MaterialTextfield materialTextfield2;
+        private MaterialSurface.ContainedButton containedButton1;
+        private MaterialSkin.Controls.MaterialButton materialButton1;
+    }
+}
