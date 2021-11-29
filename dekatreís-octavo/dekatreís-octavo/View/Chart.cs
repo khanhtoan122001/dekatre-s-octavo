d7441a52dbc0b0f4bd526cc175d79a076b2b4efc
yami-newbie <68190399+yami-newbie@users.noreply.github.com>
2021-11-29 12:52:29 +0700

    .

---
+++ dekatreís-octavo/dekatreís-octavo/View/Chart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiveCharts.WinForms;
+using LiveCharts;
+
+namespace dekatreís_octavo.View
+{
+    public partial class Chart : UserControl
+    {
+        public Chart()
+        {
+            InitializeComponent();
+        }
+    }
+}
